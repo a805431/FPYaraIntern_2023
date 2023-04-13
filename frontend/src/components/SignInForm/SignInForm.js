cd147df56036170ccf85601649d0ad2526ad0594
a805431 <gergana.ivanova@yara.com>
2023-04-13 16:38:55 +0300

    user message under sign in button implemented

--- frontend/src/components/SignInForm/SignInForm.js
+++ frontend/src/components/SignInForm/SignInForm.js
@@ -23,48 +23,60 @@ function SignInForm() {
 
   const handleEmailInputChange = (event) => {
     setEmailInput(event.target.value);
-    console.log(emailInput);
+    console.log('Email Input: ', emailInput);
   };
 
   const handlePasswordInputChange = (event) => {
     setPasswordInput(event.target.value);
+    console.log('Password Input: ', passwordInput);
   };
 
   const handleSignInUser = (event) => {
     event.preventDefault();
 
-    signInUser({email: 'daniel-505@yahoo.com', password: 'fhgruefR5$hj'})
-      .unwrap()
-      .then((fulfilled) => console.log(fulfilled))
-      .catch((rejected) => console.error(rejected));
-    console.log(results.data);
+    // signInUser({email: 'daniel-505@yahoo.com', password: 'fhgruefR5$hj'});//test line
+    signInUser({ email: emailInput, password: passwordInput });
+    // .unwrap()
+    // .then((fulfilled) => console.log(fulfilled))
+    // .catch((rejected) => console.error(rejected));
+    console.log('Response results: ', results);
   };
 
+  if (results.isLoading) {
+    content = <div>Loading...</div>;
+  } else if (results.isError) {
+    content = <div>{results.error.data.message}</div>;
+  } else if (results.status === 'fulfilled' && results.isSuccess) {
+    content = <div>Welcome, {results.data.email} !</div>;
+  }
+
+  console.log('Response results after if statement: ', results);
+
   return (
-      <Container fluid="md" className='p-5 mb-4 mt-5 bg-info rounded-3'>
-        <form>
-          <label>Email</label>
-          <p>
-            <input
-              className="emailInput"
-              type="text"
-              value={emailInput}
-              onChange={handleEmailInputChange}
-            />
-          </p>
-          <label>Password</label>
-          <p>
-            <input
-              className="password"
-              type="text"
-              value={passwordInput}
-              onChange={handlePasswordInputChange}
-            />
-          </p>
-          <Button onClick={handleSignInUser}>Sign In</Button>
-        </form>
-        {content}
-      </Container>
+    <Container fluid="md" className="p-5 mb-4 mt-5 bg-info rounded-3">
+      <form>
+        <label>Email</label>
+        <p>
+          <input
+            className="emailInput"
+            type="text"
+            value={emailInput}
+            onChange={handleEmailInputChange}
+          />
+        </p>
+        <label>Password</label>
+        <p>
+          <input
+            className="password"
+            type="password"
+            value={passwordInput}
+            onChange={handlePasswordInputChange}
+          />
+        </p>
+        <Button onClick={handleSignInUser}>Sign In</Button>
+      </form>
+      {content}
+    </Container>
   );
 }
 
